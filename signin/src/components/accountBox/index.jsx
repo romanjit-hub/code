16d6ce61c2d67ef1b1910c1d134415d9cc895287
Sruthy Samuel <sruthy.amey@gmail.com>
2023-03-01 17:23:30 -0500

    modified:   signin/src/components/accountBox/index.jsx

--- signin/src/components/accountBox/index.jsx
+++ signin/src/components/accountBox/index.jsx
@@ -37,11 +37,11 @@ const BackDrop = styled(motion.div)`
   transform: rotate(60deg);
   top: -290px;
   left: -70px;
-  background: rgb(241, 196, 15);
+  background: rgb(18, 52, 86);
   background: linear-gradient(
     58deg,
-    rgba(241, 196, 15, 1) 20%,
-    rgba(243, 172, 18, 1) 100%
+    rgba(18, 52, 86, 1) 20%,
+    rgba(18, 52, 86, 1) 100%
   );
 `;
 
